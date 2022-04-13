c9efad30c2b47c530491c5b8a95908a5aa7c6832
revveerie <revveerie@gmail.com>
2022-04-13 23:31:11 +0300

    feat: process no user photo case

--- src/components/ProfileIcon.jsx
+++ src/components/ProfileIcon.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 
+import NoPhoto from '../assets/icons/avatar.png';
+
 const ProfileIcon = ({ profile }) => {
   return (
     <>
       <div className="profile__photo">
-        <img src={profile} className="profile__photo-image" />
+        {profile != null ? (
+          <img src={profile} className="profile__photo-image" />
+        ) : (
+          <div className="profile__no-photo">
+            <img src={NoPhoto} className="profile__no-photo-image" />
+          </div>
+        )}
+        
       </div>
     </>
   );
