e4da906372f40ad7b39150ee8a17212ac7d03a08
revveerie <revveerie@gmail.com>
2022-06-12 17:56:22 +0300

    fix: fix playlist card view

--- src/components/cards/PlaylistCard.jsx
+++ src/components/cards/PlaylistCard.jsx
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 
 import NoPlaylist from "../../assets/images/no-playlist-image.jpg";
 
 const PlaylistCard = ({ name, image, index, id, creator }) => {
+  const ref = useRef(null);
+  const [width, setWidth] = useState("");
+
+  useEffect(() => {
+    setWidth(ref.current.offsetWidth);
+    window.addEventListener("resize", function () {
+      ref.current ? setWidth(ref.current.offsetWidth) : false;
+    });
+  }, []);
+
+  const mystyle = {
+    height: `${width}px`,
+  };
+
   return (
     <>
       <div className="basic__card playlist__card" key={index}>
         <Link className="basic__card-link playlist__card-link" to={`/playlist/${id}`}>
           <div className="basic__image playlist__image">
-            {image != null ? <img src={image} alt={name} /> : <img src={NoPlaylist} alt={name} />}
+            {image != null ? (
+              <img src={image} alt={name} ref={ref} style={mystyle} />
+            ) : (
+              <img src={NoPlaylist} alt={name} ref={ref} style={mystyle} />
+            )}
           </div>
         </Link>
         <div className="basic__info playlist__info">
